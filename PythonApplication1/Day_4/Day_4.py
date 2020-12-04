e5af22abc5cb0f86eb6f3954389eeb4c9c83a60c
Erik Rodriguez <luffiez123@gmail.com>
2020-12-04 22:09:15 +0100

    completed day 4

--- PythonApplication1/Day_4/Day_4.py
+++ PythonApplication1/Day_4/Day_4.py
@@ -7,42 +7,87 @@ print("######        DAY 3        ######\n")
 with open('Day_4\input.txt', 'r') as f:
     input = f.read().split("\n\n")
 
-reqFields = list(["byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"])
+reqFields = list(['byr:', 'ecl:', 'eyr:', 'hcl:', 'hgt:', 'iyr:', 'pid:']) # This is the sorted order
+validColors = ["amb","blu","brn","gry","grn","hzl","oth"]
+validChars = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
 validPassports = 0
 
-passports = list()
-
-class Passport(NamedTuple):
-    byr : str # Birth Year, 4-Digits 1920 - 2002
-    iyr : str # Issue Year, 4-Digits 2010 - 2020
-    eyr : str # Expiration Year, 4-Digits 2020 - 2030
-    hgt : str # Height, A number followed by cm (150 - 193) or in (59 - 76)
-    hcl : str # Hair Color, # followed by 6 characters 0-9 or a-f
-    ecl : str # Eye Color, exactly one of: amb, blu, gry, grn, hzl, oth
-    pid : str # Passport ID, a 9-Digits, including leading zeroes
-
+# Please don't look at this function, it's a monster :(
 def isValid(passport):
     passport = passport.replace('\n',' ')
     lines = passport.split(" ")
-    byr = between(passport, "byr:", 4)
-    iyr = between(passport, "iyr:", 4)
-    eyr = between(passport, "eyr:", 4)
-    hgt = between(passport, "hgt:", " ")
-    hcl = between(passport, "hcl:", 7)
-    ecl = between(passport, "ecl:", 3)
-    pid = between(passport, "pid:", 9)
+    lines.sort()
 
-def between(value, a, b):
-    # Find and validate before-part.
-    pos_a = value.find(a)
-    if pos_a == -1: return ""
-    # Find and validate after part.
-    pos_b = pos_a + b
-    if pos_b == -1: return ""
-    # Return middle part.
-    adjusted_pos_a = pos_a + len(a)
-    if adjusted_pos_a >= pos_b: return ""
-    return value[adjusted_pos_a:pos_b]
+    for line in lines:
+        if("byr:" in line):
+            if(yearCheck(int(line.replace("byr:","")), 1920, 2002)):
+                continue
+            else:
+                return False
+
+        if("ecl:" in line):
+            line = line.replace("ecl:","")          
+            if (line in validColors):
+                continue
+            else:
+                return False
+                    
+        if("eyr:" in line):
+            if(yearCheck(int(line.replace("eyr:","")), 2020, 2030)):
+                continue
+            else:
+                return False
+        
+        if("hcl:" in line):
+            line = line.replace("hcl:","")
+            if("#" in line):
+                line = line.replace("#","")
+            else:
+                return False
+            if any(str(char) not in validChars for char in line):
+                return False
+            else:
+                continue
+
+        if("hgt:" in line):
+            line = line.replace("hgt:","")
+            if("cm" in line):
+                line = line.replace("cm", "")
+                if(line.isnumeric() == False or int(line) < 150 or int(line) > 193):
+                    return False
+                else:
+                    continue
+            elif("in" in line):
+                line = line.replace("in", "")
+                if(line.isnumeric() == False or int(line) < 59 or int(line) > 76):
+                    return False
+                else:
+                    continue
+            else:
+                return False
+        if("iyr:" in line):
+            if(yearCheck(int(line.replace("iyr:","")), 2010, 2020)):
+                continue
+            else:
+                return False
+
+        elif("pid:" in line):
+            line = line.replace("pid:","")
+            if(line.isnumeric() and len(line) == 9):
+                continue
+            else:
+                return False
+
+        if("cid:" in line):
+            continue
+
+    return True
+
+def yearCheck(current, min, max):
+    if(len(str(current)) == 4 and current >= min and current <= max):
+        return True
+    else:
+        return False
 
 def hasFields(passport):
     if all(field in passport for field in reqFields): # Check that it has all the required fields.
@@ -50,19 +95,13 @@ def hasFields(passport):
     else:
         return False
 
-print("Passports in list: ", len(passports))
-
 for passport in input:
     passport = passport.replace('\n',' ')
-    if(hasFields(passport)):
-        passports.append(isValid(passport))
-
-#for passport in passports:
-#    passport = passport.replace('\n',' ')
-#    if (isValid(passport)):
-#        validPassports += 1
-#        print("OK")
+    if(hasFields(passport) and isValid(passport)):
+        validPassports += 1
   
 print("Valid Passports in list: ", validPassports)
+print("Invalid Passports in list: ", len(input) - validPassports)
+
 
 print("\n######  BY ERIK RODRIGUEZ  ######")
